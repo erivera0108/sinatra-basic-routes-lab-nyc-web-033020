b0d8e46d3cb2c31d5e1fa5c87d6cafed696ac75a
Edgar <edgar.a.rivera08@gmail.com>
2020-04-19 16:23:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    # get ('/name') {"My name is Edgar"}
+    get '/name' do 
+        "My name is Edgar"
+    end
+
+    # get ('/hometown') {'My hometown is NYC'} 
+    get '/hometown' do 
+        "My hometown is NYC"
+    end
+
+    # get ('/favorite-song') {"My favorite song is Clocks by coldplay"}
+    get '/favorite-song' do 
+        "My favorite song is Clocks by Coldplay"
+    end
+
 end
